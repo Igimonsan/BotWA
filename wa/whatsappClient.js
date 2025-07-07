3e333eff5fe1ecf14b0e9d7735b9eded8ef86c5d
Igimonsan <akuntb101010@gmail.com>
2025-07-07 12:55:55 +0700

    Push

--- wa/whatsappClient.js
+++ wa/whatsappClient.js
@@ -271,7 +271,7 @@ class WhatsAppClient {
             }
             //!brat
             if (lowerText.startsWith('!brats')) {
-                const url = text.substring(6).trim();
+                const url = text.substring(7).trim();
                 await this.handleBratsticker(sender, url);
                 return;
             }
